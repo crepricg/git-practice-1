824f91cb31b4e84fea8146a5e58a891a84faf703
crepric <crepric@gmail.com>
2015-08-04 09:49:05 -0500

    Create users_snippet.py

---
+++ users_snippet.py
@@ -0,0 +1,26 @@
+from google.appengine.api import users
+from google.appengine.ext import ndb
+import webapp2
+
+class UserModel(ndb.Model):
+    currentUser = ndb.StringProperty(required = True)  # OR not required, or repeated, depends on your app.
+    some_text = ndb.TextProperty()
+    some_more_test = ndb.TextProperty()
+    
+class MainHandler(webapp2.RequestHandler):
+    def get(self):
+        # Try and read who's the current user.
+        user = users.get_current_user()
+        if user:
+            # If there was a user logged in, do stuff.
+            self.response.write(user)
+            user = UserModel(currentUser = user.user_id(), text= "hey")
+            user.put()
+        else:
+            # Send the user to a login page, then come back to this request, this
+            # time a user will be present.
+            self.redirect(users.create_login_url(self.request.uri))
+
+app = webapp2.WSGIApplication([
+    ('/', MainHandler),
+], debug=True)
